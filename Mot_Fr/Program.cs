f8815d461f6a9b20bac4d2dac170425621ceda9a
Mohammed-Yanis AZOUAOU <mohammed-yanis.azouaou@edu.devinci.fr>
2025-12-12 18:03:38 +0100

    ouii

--- Mot_Fr/Program.cs
+++ Mot_Fr/Program.cs
@@ -10,7 +10,7 @@ namespace Mot_Fr
     //test
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("ziziiii");
         }
     }
 }
