7530d0341e6fe276f9fbe73c3ab86976cf289291
Mohammed-Yanis AZOUAOU <mohammed-yanis.azouaou@edu.devinci.fr>
2025-12-14 14:46:25 +0100

    ok

--- Mot_Fr/Dictionnaire.cs
+++ Mot_Fr/Dictionnaire.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace Mot_Fr
 {
@@ -13,17 +14,28 @@ namespace Mot_Fr
 
         public Dictionnaire(string fichier)
         {
-            List<string> tempMots = new List<string>();
+            List<string> tempMots = new List<string>()[26];
+            for (int i = 0; i < 26; i++)
+            {
+                mots[i] = new List<string>();
+            }
 
             using (StreamReader sr = new StreamReader(fichier, Encoding.UTF8))
             {
-                string ligne = sr.ReadLine();
-                while (ligne != null)
+                string ligne;
+                int i = 0;
+                while ((sr.ReadLine()) != null && i < 26)
                 {
                     string[] motsligne = ligne.Split(' ');
-                    tempMots.AddRange(motsligne);
+                    foreach (string mot in motsligne)
+                    {
+                        if (mot != motsligne)
+                        {
+                            this.mots[i].Add(mot.ToUpper());
+                        }
+                    }
+                    i++;
                 }
-                mots = tempMots.ToArray();
             }
 
         public string toString()
