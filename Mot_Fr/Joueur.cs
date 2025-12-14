9ce6fa44336bf0007d8009a125dfeadc8114d481
Mohammed-Yanis AZOUAOU <mohammed-yanis.azouaou@edu.devinci.fr>
2025-12-14 13:49:43 +0100

    valider

--- Mot_Fr/Joueur.cs
+++ Mot_Fr/Joueur.cs
@@ -9,34 +9,66 @@ namespace Mot_Fr
     public class Joueur
     {
         private string nom;
-        private List<string> mots_trouves;
+        private List<string> motstrouves;
         private int score;
 
         public Joueur(string nom)
         {
-            this.nom = nom;
+            if(nom == null || nom == "")
+            {
+                this.nom = "anonyme"; 
+            }
+            else
+            {
+                this.nom = nom;
+            }
             this.score = 0;
             this.mots_trouves = null;
         }
 
-        public void Add_Mot(string mot)
+        public string Nom 
         {
-
-        }
-        
-        public string toString()
-        {
-            return $"Joueur : {nom}\nScore : {score}\nmots trouvés : {mots_trouves}";
+            get { return nom; }
         }
 
-        public void Add_Score(int val)
+        public List<string> Motstrouves 
         {
+            get { return motstrouves; }
+        }
 
+        public int Score
+        {
+            get { return score; }
         }
 
         public bool Contient(string mot)
         {
+            for (int i = 0; i < this.motstrouves.Count(); i++)
+            {
+                if (motstrouves[i] == mot)
+                {
+                    return true;
+                }sz
+            }
             return false;
+        }
+
+        public void Add_Mot(string mot)
+        {
+            if(Contient(mot) == false)
+            {
+                this.motstrouves.Add(mot);
+            }
+        }
+        
+        public string toString()
+        {
+            return $"Nom du joueur : {nom}\nScore : {score}\nmots trouvés : {mots_trouves}";
+        }
+
+        public void Add_Score(int val)
+        {
+            this.score = this.score + val;
         }
     }
 }
